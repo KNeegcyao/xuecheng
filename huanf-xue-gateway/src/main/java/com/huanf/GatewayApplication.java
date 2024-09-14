8c5b2c28ad4df098c67735addeb420e9358bb793
KNeeg <2903909011@qq.com>
2024-09-14 12:55:15 +0800

    day05

---
+++ huanf-xue-gateway/src/main/java/com/huanf/GatewayApplication.java
@@ -0,0 +1,11 @@
+package com.huanf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
---
+++ huanf-xue-media/xue-media-model/src/main/java/com/huanf/domain/dto/UploadFileResultDto.java
@@ -0,0 +1,8 @@
+package com.huanf.domain.dto;
+
+import com.huanf.domain.entity.MediaFiles;
+import lombok.Data;
+
+@Data
+public class UploadFileResultDto extends MediaFiles {
+}
